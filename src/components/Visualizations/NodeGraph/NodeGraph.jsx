e45ca57438eab6114390daab6fe38ce16f2f456e
LeChristopher Jon Blackwell <codeblackwell@gmail.com>
2023-08-23 22:27:00 -0600

    base node graph rendering

--- src/components/Visualizations/NodeGraph/NodeGraph.jsx
+++ src/components/Visualizations/NodeGraph/NodeGraph.jsx
@@ -1,49 +1,55 @@
 import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
-const NodeGraph = ({ nodes, links }) => {
-    const ref = useRef();
+const generateTreeData = (layers) => {
+    const createNode = (level) => {
+        if (level >= layers) return { name: `Level ${level}`, children: [] };
+        return {
+            name: `Level ${level}`,
+            children: [createNode(level + 1), createNode(level + 1)]
+        };
+    };
+    return { name: 'Root', children: [createNode(1), createNode(1)] };
+};
+
+const NodeTree = ({ layers }) => {
+    const svgRef = useRef(null);
 
     useEffect(() => {
-        const svg = d3.select(ref.current)
-            .attr('width', 800)
-            .attr('height', 600);
+        const treeData = generateTreeData(layers);
+        const svg = d3.select(svgRef.current);
 
-        const simulation = d3.forceSimulation(nodes)
-            .force('link', d3.forceLink(links).id(d => d.id))
-            .force('charge', d3.forceManyBody())
-            .force('center', d3.forceCenter(400, 300));
+        // Define the tree layout
+        const root = d3.hierarchy(treeData);
+        const treeLayout = d3.tree().size([600, 400]);
+        const treeRoot = treeLayout(root);
 
-        const link = svg.append('g')
-            .selectAll('line')
-            .data(links)
-            .join('line')
-            .attr('stroke', '#999')
-            .attr('stroke-opacity', 0.6);
+        // Draw the links
+        const linkGenerator = d3.linkVertical()
+            .x(d => d.x)
+            .y(d => d.y);
 
-        const node = svg.append('g')
-            .selectAll('circle')
-            .data(nodes)
-            .join('circle')
+        svg.selectAll('path')
+            .data(treeRoot.links())
+            .enter()
+            .append('path')
+            .attr('d', linkGenerator)
+            .attr('stroke', 'black');
+
+        // Draw the nodes
+        svg.selectAll('circle')
+            .data(treeRoot.descendants())
+            .enter()
+            .append('circle')
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y)
             .attr('r', 5)
-            .attr('fill', '#f00');
-
-        simulation.on('tick', () => {
-            link
-                .attr('x1', d => d.source.x)
-                .attr('y1', d => d.source.y)
-                .attr('x2', d => d.target.x)
-                .attr('y2', d => d.target.y);
-
-            node
-                .attr('cx', d => d.x)
-                .attr('cy', d => d.y);
-        });
-    }, [nodes, links]);
+            .attr('fill', 'blue');
+    }, [layers]);
 
     return (
-        <svg ref={ref}></svg>
+        <svg ref={svgRef} width="800" height="600"></svg>
     );
 };
 
-export default NodeGraph;
+export default NodeTree;
