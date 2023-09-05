5e29f9aaec68184d2827e0c040314f21c90d0226
LeChristopher Blackwell <codeblackwell@gmail.com>
2023-09-05 12:13:54 -0700

    server wip

--- server.js
+++ server.js
@@ -28,11 +28,105 @@ app.get('/', (req, res) => {
 
 // Example route to fetch all terms (just for demonstration)
 app.get('/terms', async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 100;
+
     try {
-        const terms = await Term.findAll();
+        const terms = await Term.findAll({
+            offset: (page - 1) * pageSize,
+            limit: pageSize
+        });
         res.json(terms);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch terms' });
+    }
+});
+
+// Fetch Term by ID
+app.get('/term/:id', async (req, res) => {
+    try {
+        const term = await Term.findByPk(req.params.id);
+        if (term) {
+            res.json(term);
+        } else {
+            res.status(404).json({ error: 'Term not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch term' });
+    }
+});
+
+// Search Terms by Name
+app.get('/terms/search', async (req, res) => {
+    const termName = req.query.name;
+    if (!termName) {
+        return res.status(400).json({ error: 'Name parameter is required' });
+    }
+
+    try {
+        const terms = await Term.findAll({
+            where: {
+                name: {
+                    [Op.like]: `%${termName}%`
+                }
+            }
+        });
+        res.json(terms);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to search terms' });
+    }
+});
+// Get Term by ID
+app.get('/api/terms/:term_id', async (req, res) => {
+    try {
+        const term = await Term.findByPk(req.params.term_id);
+        if (term) {
+            res.json(term);
+        } else {
+            res.status(404).json({ error: 'Term not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// List All Terms (with Pagination)
+app.get('/api/terms', async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 50;
+        const offset = (page - 1) * limit;
+        const terms = await Term.findAll({ offset, limit });
+        res.json(terms);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/api/terms/random', async (req, res) => {
+    try {
+        const term = await Term.findOne({ order: sequelize.random() });
+        if (term) {
+            res.json(term);
+        } else {
+            res.status(404).json({ error: 'No terms available' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Autocomplete Term (assuming there's a 'term' field in the Term model)
+app.get('/api/terms/autocomplete', async (req, res) => {
+    try {
+        const partialTerm = req.query.query;
+        const terms = await Term.findAll({
+            where: { term: { [Op.like]: `${partialTerm}%` } },
+            limit: 10 // Limiting to 10 results for autocomplete
+        });
+        res.json(terms);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
