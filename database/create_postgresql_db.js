8c257053d534d8335b6a51e4053c02469c04d27c
LeChristopher Blackwell <codeblackwell@gmail.com>
2023-09-06 09:40:56 -0700

    snowflake schema import  wip

--- database/create_postgresql_db.js
+++ database/create_postgresql_db.js
@@ -7,7 +7,7 @@ const config = {
     host: 'localhost',
     user: process.env.ETYMOLOGY_DB_USER,
     password: process.env.ETYMOLOGY_DB_PASSWORD,
-    database: 'postgres' // default database
+    database: 'etymologydb'
 };
 
 const client = new Client(config);
@@ -20,7 +20,7 @@ async function createDatabaseAndTables() {
         // Switch to the new database
         client.end();
         config.database = 'etymologydb';
-        client = new Client(config);
+        let client = new Client(config);
         await client.connect();
 
         // Create tables
@@ -71,12 +71,19 @@ async function importCSVtoPostgreSQL(directoryPath) {
                 const readStream = fs.createReadStream(filePath);
                 readStream.pipe(csv())
                     .on('data', async (row) => {
-                        // Adjust this part based on the CSV structure and the table structure
+                        // Insert data into dimension tables
+                        await client.query('INSERT INTO Languages(lang) VALUES($1) ON CONFLICT (lang) DO NOTHING', [row.lang]);
+                        await client.query('INSERT INTO GroupTags(group_tag) VALUES($1) ON CONFLICT (group_tag) DO NOTHING', [row.group_tag]);
+                        await client.query('INSERT INTO ParentTags(parent_tag) VALUES($1) ON CONFLICT (parent_tag) DO NOTHING', [row.parent_tag]);
+                        await client.query('INSERT INTO RelatedTerms(related_term, reltype, related_lang_id) VALUES($1, $2, (SELECT lang_id FROM Languages WHERE lang = $3)) ON CONFLICT (related_term) DO NOTHING', [row.related_term, row.reltype, row.related_lang]);
+
+                        // Insert data into fact table
                         const query = `
-                            INSERT INTO your_table_name( /* columns */ )
-                            VALUES( /* values */ );
+                            INSERT INTO Terms(term_id, term, position, lang_id, group_tag_id, parent_tag_id, parent_position, reltype, related_term_id)
+                            VALUES($1, $2, $3, (SELECT lang_id FROM Languages WHERE lang = $4), (SELECT group_tag_id FROM GroupTags WHERE group_tag = $5), (SELECT parent_tag_id FROM ParentTags WHERE parent_tag = $6), $7, $8, (SELECT related_term_id FROM RelatedTerms WHERE related_term = $9))
+                            ON CONFLICT (term_id) DO NOTHING;
                         `;
-                        const values = [ /* values from row */ ];
+                        const values = [row.term_id, row.term, row.position, row.lang, row.group_tag, row.parent_tag, row.parent_position, row.reltype, row.related_term];
                         await client.query(query, values);
                     })
                     .on('end', () => {
