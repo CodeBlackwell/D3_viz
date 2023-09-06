09e40d1148be2c37f1ba8b32d6160c94fe2fcf60
LeChristopher Blackwell <codeblackwell@gmail.com>
2023-09-06 10:05:35 -0700

    wip

--- database/create_postgresql_db.js
+++ database/create_postgresql_db.js
@@ -11,50 +11,73 @@ let client = new Client({
 });
 async function createDatabaseAndTables() {
     try {
+        // Check if the database exists
+        const dbExistsResult = await client.query("SELECT 1 FROM pg_database WHERE datname='etymologydb'");
+        if (dbExistsResult.rowCount > 0) {
+            console.log('Database etymologydb already exists. Deleting...');
+            await client.query('DROP DATABASE etymologydb;');
+            console.log('Database etymologydb deleted successfully.');
+        }
+
+        // Create the database
         await client.query('CREATE DATABASE etymologydb;');
         console.log('Database etymologydb created successfully.');
 
-        // Switch to the new database
-        client.end();
-        config.database = 'etymologydb';
-        client = new Client(config);
+        // Close the initial connection
+        await client.end();
+
+        // Update the connectionString to connect to the newly created etymologydb
+        const newConnectionString = `postgresql://${process.env.ETYMOLOGY_DB_USER}:${process.env.ETYMOLOGY_DB_PASSWORD}@localhost:5432/etymologydb`;
+        client = new Client({
+            connectionString: newConnectionString
+        });
         await client.connect();
 
-        // Create tables
         await client.query(`
-            CREATE TABLE Languages (
+            CREATE TABLE languages (
                 lang_id SERIAL PRIMARY KEY,
-                lang VARCHAR(255) NOT NULL
+                lang_name TEXT UNIQUE NOT NULL
             );
-            CREATE TABLE GroupTags (
+
+            CREATE TABLE reltypes (
+                reltype_id SERIAL PRIMARY KEY,
+                reltype_name TEXT UNIQUE NOT NULL
+            );
+
+            CREATE TABLE group_tags (
                 group_tag_id SERIAL PRIMARY KEY,
-                group_tag VARCHAR(255) NOT NULL
+                group_tag_name TEXT UNIQUE NOT NULL
             );
-            CREATE TABLE ParentTags (
+
+            CREATE TABLE parent_tags (
                 parent_tag_id SERIAL PRIMARY KEY,
-                parent_tag VARCHAR(255) NOT NULL
-            );
-            CREATE TABLE RelatedTerms (
-                related_term_id SERIAL PRIMARY KEY,
-                related_term VARCHAR(255) NOT NULL,
-                reltype VARCHAR(255),
-                related_lang_id INTEGER REFERENCES Languages(lang_id)
-            );
-            CREATE TABLE Terms (
-                term_id SERIAL PRIMARY KEY,
-                term VARCHAR(255) NOT NULL,
-                position INTEGER,
-                lang_id INTEGER REFERENCES Languages(lang_id),
-                group_tag_id INTEGER REFERENCES GroupTags(group_tag_id),
-                parent_tag_id INTEGER REFERENCES ParentTags(parent_tag_id),
-                parent_position INTEGER
+                parent_tag_name TEXT UNIQUE NOT NULL
             );
         `);
-        console.log('Tables created successfully.');
+        console.log('Dimension tables created successfully.');
+
+        // Create the fact table
+        await client.query(`
+            CREATE TABLE terms (
+                term_id TEXT NOT NULL,
+                term TEXT,
+                lang_id INTEGER REFERENCES languages(lang_id),
+                reltype_id INTEGER REFERENCES reltypes(reltype_id),
+                related_term_id TEXT,
+                related_lang_id INTEGER REFERENCES languages(lang_id),
+                position REAL,
+                group_tag_id INTEGER REFERENCES group_tags(group_tag_id),
+                parent_tag_id INTEGER REFERENCES parent_tags(parent_tag_id),
+                parent_position REAL
+            );
+        `);
+        console.log('Fact table created successfully.');
+
     } catch (err) {
         console.error('Error creating database or tables:', err);
     }
 }
+
 
 async function importCSVtoPostgreSQL(directoryPath) {
     try {
